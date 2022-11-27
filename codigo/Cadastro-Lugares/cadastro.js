37d1266fabf6486ca78ecc6d4642793cb4bea324
Goulart <113138191+Goulart2@users.noreply.github.com>
2022-11-27 20:24:11 -0300

    Add files via upload

--- codigo/Cadastro-Lugares/cadastro.js
+++ codigo/Cadastro-Lugares/cadastro.js
@@ -1,5 +1,29 @@
 function cadastroFeito(){
     let confirma = confirm('Confirmar o cadastro?');
-    if(confirma) alert('Cadastro Feito com Sucesso!');
+
+    if(confirma) {
+
+        var arrayPessoas=[]
+
+    if(JSON.parse(localStorage.getItem("pessoas")) != null){
+        arrayPessoas.push(JSON.parse(localStorage.getItem("pessoas")));
+    }   
+    var nome = document.getElementById("inputNome").value;
+    var cnpj = document.getElementById("inputCnpj").value;
+    var cidade = document.getElementById("inputCidade").value;
+    var telefone = document.getElementById("inputTelefone").value;
+    var email = document.getElementById("inputEmail").value;
+    var endereco = document.getElementById("inputEndereco").value;
+    
+    var pessoa = {nome: nome, cnpj: cnpj, cidade: cidade, telefone: telefone, email: email, endereco: endereco};
+    arrayPessoas.push(pessoa);
+    var pessoaJson = JSON.stringify(arrayPessoas);
+    localStorage.setItem("pessoas", pessoaJson);
+    
+    alert('Cadastro Feito com Sucesso!');
+        
+    }
     else alert('Cadastro cancelado')
 }
+
+
